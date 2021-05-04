f0fa8f47b6399a096035641e06f0c2bb24809e11
Irvin Szeto <irvins@stanford.edu>
2021-05-04 08:33:41 -0700

    updateing instructions and adding partner set up zip here

--- pages/instructions.php
+++ pages/instructions.php
@@ -7,18 +7,15 @@ namespace Stanford\AIMIHome;
 <div style='margin:20px 0; max-width:98%'>
     <h4>Stanford AIMI Partner Instructions</h4>
 
-    <p>In order to use this module in conjunction with the Stanford AIMI EM please download this <br><a target="_blank" href="<?=$module->getUrl("StanfordAIMIPhoneHome.REDCap.xml",true,true);?>">REDCap Project XML (right-click and "Save Link As")</a> to create the required instruments and set up the repeating instances</p>
+    <p>To add a new partner please provide them with these setup instructions and start up XML <br><a target="_blank" href="<?=$module->getUrl("REDCap_AIMI_SETUP.zip",true,true);?>">REDCap_AIMI_SETUP.zip (right-click and "Save Link As")</a></p>
 
-    <p>Once a Partner Institution has completed a DRA/Business Agreement, Create a unique token for them using generator (TBD) and add a record to the Partners Instrument.</p>
+    <p>Once the partner has completed set up of their REDCap AIMI Project and DRA/Business Agreement, create a unique token for them using generator (TBD) and add a record to the Partners Instrument in this project.</p>
     <p>Then provide the Partner Institution with the [partner_token] and the [api_endpoint] as shown below.</p>
 
     <br>
     <br>
 
-
-
-
-    <p>This endpoint will be used by the partner institution to push results from using pretrained models back to this project for Stanford analysis</p>
+    <p>This endpoint will be used by the partner institution to push prediction results + partner "ground truths" from using pretrained models back to this project for Stanford analysis</p>
     <h4>Stanford Partner Results Uploader Endpoint</h4>
     <pre><?php echo $module->getUrl("endpoints/uploader.php",true, true ) ?></pre>
 
